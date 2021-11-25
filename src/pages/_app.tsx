bac882bf700651391eacb6493528bbf28d9975cc
urgobalt <lukecastellan165@gmail.com>
2021-11-25 16:07:14 +0100

    Redux setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.sass'
 import type { AppProps } from 'next/app'
+import {Provider} from 'react-redux'
+import {store} from '@lib/redux-store'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -4,5 +4,13 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+    switch(req.method) {
+        case "GET": {
+          return res.
+        }
+        default: {
+          return res.status(405).json({})
+        }
+    }
   res.status(200).json({error: "Incorrect username or password"})
 }
